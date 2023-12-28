5bd4fc3b0c052bb48374525054d5adb22313aaec
aishwarya Dhumal <aishudhumal01@gmail.com>
2023-12-28 01:34:04 -0500

    add layout in index file

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
+
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import LandingPage from '@/component/landingPage'
+import LayoutPage from '@/component/layout'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,3 +13,6 @@ export default function Home() {
           </>
   )
 }
+Home.getLayout = function getLayout(page) {
+  return <LayoutPage>{page}</LayoutPage>;
+};
